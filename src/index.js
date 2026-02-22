4201d3fe3c1c1e3c3826fb77f11cb4b86c94df07
[Deepak kumar] <[kdeepak41369@gmail.com]>
2026-02-23 01:25:43 +0530

    setup bodyparser

--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 const express = require('express');
+// require('dotenv').config();
+
+const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
 
@@ -6,10 +9,14 @@ const setupAndStartServer = async () => {
 
     // Create an Express object
     const app = express();
-    const PORT = 3000;
+    // const PORT = 3000;
+
+    app.use(bodyParser.json());
+    app.use(bodyParser.urlencoded({ extended: true }));
+
     app.listen(PORT, () => {
         console.log(`Server is running on port ${PORT}`);
-        console.log(process.env);
+        // console.log(process.env.PORT);
     });
 
 
