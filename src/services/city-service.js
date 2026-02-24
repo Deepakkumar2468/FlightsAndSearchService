80938736a08340ca9f6d14e6516f8a957b278cde
[Deepak kumar] <[kdeepak41369@gmail.com]>
2026-02-24 23:47:01 +0530

    added city service

---
+++ src/services/city-service.js
@@ -0,0 +1,58 @@
+const { CityRepository } = require('../repository/index');
+
+class CityService {
+    constructor() {
+        this.cityRepository = new CityRepository();
+    }
+    
+    async createCity(data) {
+        try {
+            const city = await this.cityRepository.createCity(data);
+            return city;
+        } catch (error) {
+            console.error('Error creating city:', error);
+            throw error;
+        }
+    }               
+     async updateCity(cityId, data) {
+        try {
+            const result = await this.cityRepository.updateCity(cityId, data);
+            return result;
+        } catch (error) {
+            console.error('Error updating city:', error);
+            throw error;
+        }
+    }
+
+    async deleteCity(id) {
+        try {
+            const result = await this.cityRepository.deleteCity(id);
+            return result;
+        } catch (error) {
+            console.error('Error deleting city:', error);
+            throw error;
+        }
+    }
+
+    async getCityById(id) {
+        try {
+            const city = await this.cityRepository.getCityById(id);
+            return city;
+        } catch (error) {
+            console.error('Error fetching city by ID:', error);
+            throw error;
+        }
+    }
+
+    async getAllCities() {
+        try {
+            const cities = await this.cityRepository.getAllCities();
+            return cities;
+        } catch (error) {
+            console.error('Error fetching all cities:', error);
+            throw error;
+        }
+    }
+}
+
+module.exports = CityService;
